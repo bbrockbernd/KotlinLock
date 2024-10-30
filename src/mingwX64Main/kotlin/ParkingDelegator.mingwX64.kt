fe85df7744dd7590e75d119ef731a7b41d7cc563
Bob.Brockbernd <bobbrockbernd@gmail.com>
2024-10-30 14:58:39 +0100

    Windows impl

--- src/mingwX64Main/kotlin/ParkingDelegator.mingwX64.kt
+++ src/mingwX64Main/kotlin/ParkingDelegator.mingwX64.kt
@@ -1,20 +1,38 @@
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.toCPointer
+import kotlinx.cinterop.*
+import platform.windows.INFINITE
 import platform.windows.UINT64Var
 import platform.windows.WaitOnAddress
+import platform.windows.WakeByAddressSingle
 
 @OptIn(ExperimentalForeignApi::class)
 actual class ParkingDelegator actual constructor() {
     actual fun createFutexPtr(): Long {
-        TODO("Not yet implemented")
+        val signal = nativeHeap.alloc<UINT64Var>()
+        signal.value = 0u
+        return signal.ptr.toLong()
     }
 
+    // From https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitonaddress
     actual fun wait(futexPrt: Long, notifyWake: (result: Int) -> Unit) {
-//        val ptr = futexPrt.toCPointer<UINT64Var>()
-//        WaitOnAddress(ptr, , 8, )
+        val cPtr = futexPrt.toCPointer<UINT64Var>() ?: throw IllegalStateException("Could not create C Pointer from futex ref")
+        
+        val undesiredValue = nativeHeap.alloc<UINT64Var>()
+        undesiredValue.value = 0u
+        var capturedValue = cPtr.pointed.value.toInt()
+        var result = 0
+        while (capturedValue == 0) {
+            result = WaitOnAddress(cPtr, undesiredValue.ptr, 8u, INFINITE)
+            capturedValue = cPtr.pointed.value.toInt()
+        }
+        notifyWake(result)
+        
+        nativeHeap.free(undesiredValue)
+        nativeHeap.free(cPtr)
     }
 
     actual fun wake(futexPrt: Long): Int {
-        TODO("Not yet implemented")
+        WakeByAddressSingle(futexPrt.toCPointer<UINT64Var>())
+        // windows doesn't return a success or fail status.
+        return 0
     }
 }
