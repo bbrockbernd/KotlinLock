8a453b7ef510ac8df077c3978c380d7c2fc173cc
Bob.Brockbernd <bobbrockbernd@gmail.com>
2024-11-23 11:50:27 +0200

    Linux deadlock fix

--- src/linuxMain/kotlin/NativeParkingDelegator.linux.kt
+++ src/linuxMain/kotlin/NativeParkingDelegator.linux.kt
@@ -17,17 +17,14 @@ internal actual object NativeParkingDelegator: ParkingDelegator {
     }
 
     actual override fun wait(futexPrt: Long): Boolean {
-        println("__Wait on: ${futexPrt % 100}")
         val cPtr = futexPrt.toCPointer<UIntVar>() ?: throw IllegalStateException("Could not create C Pointer from futex ref")
         val result = syscall(SYS_futex.toLong(), futexPrt, FUTEX_WAIT, 0u, NULL)
-        println("__Woken up on: ${futexPrt % 100} with result: $result")
         val interrupted = result.toInt() == EINTR
         nativeHeap.free(cPtr)
         return interrupted
     }
 
     actual override fun wake(futexPrt: Long): Int {
-        println("__Waking: ${futexPrt % 100}")
         //Returns n threads woken up (needs to be 1)
         val result = syscall(SYS_futex.toLong(), futexPrt, FUTEX_WAKE, 1u, NULL).toInt()
         return if (result == 1) 0 else -1
